938d3e0f8e14919e1920f249963cc8aaeeaf1483
Wallace Biscaro <wrbiscaro@hotmail.com>
2022-10-07 00:35:14 -0300

    Initial commit

---
+++ src/main/java/com/wallacebiscaro/worldcupprometheusgrafana/WorldcupPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.wallacebiscaro.worldcupprometheusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupPrometheusGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallacebiscaro/worldcupprometheusgrafana/WorldcupPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacebiscaro.worldcupprometheusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
